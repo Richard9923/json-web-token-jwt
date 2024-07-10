585b0b3ffa47c441a1ff562db3a9c0aaa658a962
Richard <programmingislife23@gmail.com>
2024-07-10 15:17:19 -0300

    criando end points

--- src/server.js
+++ src/server.js
@@ -3,4 +3,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { verifyu } = require('jsonwebtoken');
-const { hash, compare } = require('bcryptjs';)
+const { hash, compare } = require('bcryptjs');
+
+
+// 1. Register a user
+// 2. Login a user
+// 3. Logout a user
+// 4. Setup a proptected route
+// 5. Get a new accesstoken with a refresh token
