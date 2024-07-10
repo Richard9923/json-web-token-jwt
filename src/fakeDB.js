61703f446b6107ddb0816456983f28390c4fb04e
Richard <programmingislife23@gmail.com>
2024-07-10 15:56:58 -0300

    criando registro de usuário

---
+++ src/fakeDB.js
@@ -0,0 +1,7 @@
+exports.fakeDB = [
+    {
+        id: 0, 
+        email: 'email@gmail.com',
+        password: '$2a$10$ouqsPMkj9.W5Z46QkQ6EGut.O6d/afSP29IZVEaey9AWKs89qM036' // tested
+    }
+];
--- src/server.js
+++ src/server.js
@@ -5,9 +5,59 @@ const cors = require('cors');
 const { verifyu } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
 
+const { fakeDB } = require('./fakeDB.js');
+
 
 // 1. Register a user
 // 2. Login a user
 // 3. Logout a user
 // 4. Setup a proptected route
 // 5. Get a new accesstoken with a refresh token
+
+const server = express();
+
+// Use express middleware for easier cookie handling
+server.use(cookieParser());
+server.use(
+    cors({
+        orgin: 'http://localhost:8000',
+        credentials: true
+    })
+);
+
+// Needed to be able to read body data
+server.use(express.json()); // to support JSON-encoded bodies
+server.use(express.urlencoded({extended: true})); // Support URL-encoded bodies
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server listening on PORT: ${process.env.PORT}`)
+})
+
+// 1. Register a user 
+server.post('/register', async (req, res) => {
+    const { email, password} = req.body; 
+
+    try {
+        // 1. Check if user exist
+        
+        const user = fakeDB.find(user => user.email === email);
+        if (user) throw new Error("User already exist");
+        // 2 if not user exist, hash the password
+
+        const hashedPassword = await hash(password, 10);
+        console.log(hashedPassword);
+        // 3. Insert the user in "database"
+        fakeDB.push({
+            id: fakeDB.length,
+            email,
+            password: hashedPassword
+        })
+
+        res.send({message: 'User Created' });
+        console.log(fakeDB);
+    } catch(err) {
+        res.send({
+            error: `${err.message}`,
+        })
+    }
+})
